89841ba0b8848ef447a8dd225afadfc34a540a29
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-09-04 02:21:56 +0300

    task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,8 @@
 '''Basic authentication module'''
 from api.v1.auth.auth import Auth
 import base64
-from typing import Tuple
+from typing import Tuple, TypeVar
+myUser = TypeVar('User')
 
 
 class BasicAuth(Auth):
@@ -81,3 +82,27 @@ class BasicAuth(Auth):
         if ":" not in decoded_base64_authorization_header:
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(":", 1))
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> myUser:
+        """
+        Returns the User object based on the email and password.
+
+        Args:
+            user_email (str): The email to search for.
+            user_pwd (str): The password to verify.
+
+        Returns:
+            myUser: The User object if it exists and
+            the password is correct, None otherwise.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        users = myUser.search({"email": user_email})
+        if len(users) == 0:
+            return None
+        if not users[0].is_valid_password(user_pwd):
+            return None
+        return users[0]
