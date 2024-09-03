3a8ce49653c304ab03930c75438efaa9319d725f
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-09-03 21:52:09 +0300

    task 1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,14 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+def unauthorized_endpoint():
+    """
+    Handles GET requests to /api/v1/unauthorized.
+
+    Returns:
+      - A 401 Unauthorized response.
+    """
+    abort(401)
