dfd1ec8cda3b838102cc425c2a02eaf99d89ae32
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-09-04 01:16:53 +0300

    Task 6

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,13 @@
 from api.v1.auth.auth import Auth
 
+
 class BasicAuth(Auth):
-    '''Basic Auth class'''
-    pass
+    """
+    Basic authentication class.
+    """
+
+    def __init__(self):
+        """
+        Initializes a new BasicAuth instance.
+        """
+        super().__init__()
