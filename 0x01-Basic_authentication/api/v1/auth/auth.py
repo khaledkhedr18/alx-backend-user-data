614f170de964f7e217355efa341b2177845918af
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-09-03 23:45:16 +0300

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -31,7 +31,10 @@ class Auth:
         Returns:
             bool: True if authentication is required, False otherwise.
         """
-        return False
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path in excluded_paths or path.rstrip('/') in excluded_paths:
+            return False
 
     def authorization_header(self, request=None) -> str:
         """
