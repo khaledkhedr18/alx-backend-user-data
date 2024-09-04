6e9d270a531dac15ac220b454471bea9cc17d7e4
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-09-04 23:18:06 +0300

    task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Session authentication module'''
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    '''Session authentication class'''
+
+    def __init__(self) -> None:
+        super().__init__()
