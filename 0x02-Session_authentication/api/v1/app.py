1dd08f5b594a76c8f675eb80ad13ecfd6464fc34
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-09-05 00:01:58 +0300

    task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -51,9 +51,9 @@ def before_request():
                       '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
-        abort(401)
-    if auth.authorization_header(request) is None:
+    auth_header = auth.authorization_header(request)
+    session_cookie = auth.session_cookie(request)
+    if auth_header is None and session_cookie is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
